865fe97c111240ed8f812a314902ff7edc8f3381
deckel <deckel.delange@gmail.com>
2024-02-01 22:51:09 +0800

    Added readable position

--- Square.py
+++ Square.py
@@ -3,18 +3,35 @@ class Square:
     def __init__(self, x, y, piece) -> None:
         self.x = x
         self.y = y
+
+        self.position = self.readable_position()
+
         self.piece = piece
-        promotion = self.promotion()
 
     def __repr__(self):
         class_name = type(self).__name__
-        return f"{class_name}(x={self.x}, y={self.self.y}, piece={self.piece})"
+        return f"{class_name}({self.position}, piece={self.piece})"
 
-    def promotion(self):
-        if (self.y == 7 and self.piece.color == 'white') or (self.y == 0 and self.piece.color == 'black'):
-            return True
-        else:
-            return False
+    def readable_position(self):
+        x_map = {
+            0:'a',
+            1:'b',
+            2:'c',
+            3:'d',
+            4:'e',
+            5:'f',
+            6:'g',
+            7:'h'
+        }
+
+        return f"{x_map.get(self.x)}{self.y + 1}"
+
+
+    # def promotion(self):
+    #     if (self.y == 7 and self.piece.color == 'white') or (self.y == 0 and self.piece.color == 'black'):
+    #         return True
+    #     else:
+    #         return False
 
     
 
