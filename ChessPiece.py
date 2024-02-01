3ebdebff21a03ba98e8518fbe9cb0f39de3e238d
deckel <deckel.delange@gmail.com>
2024-02-01 22:51:22 +0800

    Class restructure

--- ChessPiece.py
+++ ChessPiece.py
@@ -1,32 +1,31 @@
 from abc import ABC, abstractmethod
+from Square import Square
 
-class ChessPiece:
-    def __init__(self, x, y, color, type) -> None:
-        self.x = x
-        self.y = y
-        self.piece = type,
+class ChessPiece(Square):
+    def __init__(self, color) -> None:
+        # self.x = x
+        # self.y = y
         self.color = color,
         self.legal_moves = self.calculate_legal_moves()
 
     def __repr__(self):
         class_name = type(self).__name__
-        return f"{class_name}(pos_x={self.x}, po_y={self.self.y}, piece={self.type})"
+        return f"{class_name}(color={self.color})"
 
     def calculate_legal_moves(self):
-        return None
+        pass
 
     @abstractmethod
     def move():
         pass
 
     @abstractmethod
-    @staticmethod
     def taken():
         pass
 
     def promote(self):
         print("Choose piece to promote too@")
-        return None
+        pass
 
     
 
