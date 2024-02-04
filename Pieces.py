a19b26335ca761062bc4a0d79b47b307b7100c6d
deckel <deckel.delange@gmail.com>
2024-02-04 13:05:03 +0800

    Added all piece movements

--- Pieces.py
+++ Pieces.py
@@ -34,26 +34,72 @@ class Pawn(ChessPiece):
         return available_moves
 
 class Rook(ChessPiece):
-    def available_moves(self):
+    def available_moves(self, board):
         available_moves = []
+        # define rook moves
+        for i in range(len(board.squares)+1):
+            available_moves.append((self.x, i)) # all squares on column
+            available_moves.append((i, self.y)) # all squares on row
+
         return available_moves
 
 class Knight(ChessPiece):
-    def available_moves(self):
-        available_moves = []
+    def available_moves(self, board):
+        # just hardcode all the jumps
+        available_moves = [
+            (self.x + 2, self.y + 1), (self.x + 2, self.y - 1),
+            (self.x - 2, self.y + 1), (self.x - 2, self.y - 1),
+            (self.x + 1, self.y + 2), (self.x + 1, self.y - 2),
+            (self.x - 1, self.y + 2), (self.x - 1, self.y - 2)
+        ]
+
         return available_moves
 
 class Bishop(ChessPiece):
-    def available_moves(self):
+    def available_moves(self, board):
         available_moves = []
+        # Define directions for diagonal movement
+        directions = [(-1, 1), (1, 1), (-1, -1), (1, -1)]
+
+        for dx, dy in directions:
+            new_x, new_y = self.x, self.y
+            for _ in range(7):  # Maximum 7 squares along the diagonal
+                new_x, new_y = new_x + dx, new_y + dy
+                available_moves.append((new_x, new_y))
+        
         return available_moves
 
 class Queen(ChessPiece):
-    def available_moves(self):
+    def available_moves(self, board):
         available_moves = []
+
+        # Define directions for diagonal movement
+        directions = [(-1, 1), (1, 1), (-1, -1), (1, -1)]
+
+        for dx, dy in directions:
+            new_x, new_y = self.x, self.y
+            for _ in range(7):  # Maximum 7 squares along the diagonal
+                new_x, new_y = new_x + dx, new_y + dy
+                available_moves.append((new_x, new_y))
+
+        # define horizontal directions
+        for i in range(len(board.squares)+1):
+            available_moves.append((self.x, i)) # all squares on column
+            available_moves.append((i, self.y)) # all squares on row
+
         return available_moves
 
 class King(ChessPiece):
     def available_moves(self):
-        available_moves = []
+        # Hard code basic king moves
+        available_moves = [
+            (-1, 0), 
+            (-1, 1),
+            (-1, -1),
+            (0, -1), 
+            (1, -1), 
+            (1, 0),
+            (1, 1)
+            (0, 1),
+        ]
         return available_moves
