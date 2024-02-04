11b411716711a552998a2300861c1a418e671c20
deckel <deckel.delange@gmail.com>
2024-02-04 11:30:58 +0800

    Added pawn available moves

--- Pieces.py
+++ Pieces.py
@@ -1,14 +1,47 @@
 from ChessPiece import ChessPiece
 
-class Pawn(ChessPiece):    
-    def move(self):
-        print("Pawn moves forward.")
+class Pawn(ChessPiece):
+    def available_moves(self, board):
+        available_moves = []
+
+        # define direction
         if self.color == 'white':
-            self.y += 1
-        if self.color == 'black':
-            self.y -= 1
+            direction = +1
+        else:
+            direction =-1
+        
+        # define double move
+        if self.y == 1 and self.color == 'white':
+            available_moves.append((self.x, self.y + 2*direction))
+        if self.y == 6 and self.color == 'black':
+            available_moves.append((self.x, self.y + 2*direction))
+    
+        # define single move
+        #TODO: technically this logic could live in the ChessPiece class as a legal move
+        # but it kind of is attribute of only the pawn
+        if not board.squares[self.y+1*direction][self.x].piece:
+            available_moves.append((self.x, self.y + 1*direction))
+        
+        # define diagnoal capture
+        #TODO: technically this logic could live in the ChessPiece class as a legal move
+        # but it kind of is attribute of only the pawn
+        if board.squares[self.y+1*direction][self.x+1].piece:
+            available_moves.append((self.x+1, self.y+1*direction)) # caprure to the right
+
+        if board.squares[self.y+1*direction][self.x-1].piece:
+            available_moves.append((self.x-1, self.y+1*direction)) # caprure to the left
+        
+        return available_moves
 
 class Rook(ChessPiece):
+    def available_moves(self):
+        moves = []
+
+        moves += self.x
+
+        return moves
+
+
     def move(self):
         print("Rook moves horizontally or vertically.")
 
